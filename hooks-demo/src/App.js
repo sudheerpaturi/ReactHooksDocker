b9ac3cd55ed11e2f01864dc89b0d152a0aa3e4f5
Sudheer Paturi <sudheerpaturi@gmail.com>
2019-08-25 16:53:04 -0400

    Code to demonstrate the usage of useState react hooks

--- hooks-demo/src/App.js
+++ hooks-demo/src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect (() => {
+    document.title = `You clicked ${count} times`;
+  });
+
+  const handleButtonClick = () => {
+    setCount(count + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={handleButtonClick}>
+        Hello there!
+      </button>
+      <p>Count is {count}</p>
+      <button onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
+      <button onClick={() => setCount(prevCount => prevCount - 1)}>-</button>
+    </>
+    
   );
 }
 
